a58c12da2e0214a7fb6dc8212144f1a21b0f424b
Pedro Matos <38743714+Pedro-Martes@users.noreply.github.com>
2022-10-05 12:39:43 -0300

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://pedro-martes.github.io/IgniteHyper/',
   plugins: [react()]
 })
